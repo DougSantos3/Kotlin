a1eb4a2007ff2d3c48f4c9dc036f08ab817f4627
douglas <douglasmartinssantos7@gmail.com>
2024-08-14 09:45:50 -0300

    jdbc

---
+++ src/DataBase/jdbc.kt
@@ -0,0 +1,19 @@
+package br.com.vraptor.bitBank.banco
+
+class jdbc {
+
+    /*O JDBC(Java Database Connectivity) é uma API do Java que permite que aplicações interajam com bancos de dados
+    relacionais. Essa API fornece um padrão para estabelecer conexões com bancos de dados, enviar consultas SQL, obter
+    resultados e manipular dados armazenados em bancos de dados.
+
+    O JDBC tem como objetivo principal fornecer uma camada de abstração entre o código Java e os detalhes específicos
+    do banco de dados utilizado, permitindo que as pessoas desenvolvedoras escrevam código de acesso a banco de dados
+    independente do sistema de gerenciamento de banco que estão usando.
+
+    Para conhecer mais sobre o JDBC e verificar mais exemplos de utilização, recomendamos estes dois artigos:
+
+    Conhecendo o JDBC
+    https://www.alura.com.br/artigos/conhecendo-o-jdbc?_gl=1*1b3mw9z*_ga*MTk3MzI3MjkxMi4xNzAxNDI4MDk5*_ga_1EPWSW3PCS*MTcwMzY4MjI2OC40My4xLjE3MDM2ODQ1NDYuMC4wLjA.*_fplc*bFlVVnk5VDAyazhTOFVRJTJCMnN0Q0hqaUhVdFY0U3BTQjVPdGcxMXdZakV2RjJPN0pyNnZHNXBFaWFTQU1Wc2ZqM0dURmRvVUFRcGpJRWVQTUlHNmVyc3FxZTg0dVZSdmZtSmdjT2c4STlRc0hubnRZZzlhMFI3YUQ2MDJ2N0ElM0QlM0Q.
+
+    Persistência com Kotlin utilizando JDBC*/
+}
