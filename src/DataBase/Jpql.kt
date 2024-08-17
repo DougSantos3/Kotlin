9c8efb99808089a5a56ba65260f25511ccbd9e52
douglas <douglasmartinssantos7@gmail.com>
2024-08-17 10:19:07 -0300

    jpql 2

--- src/DataBase/Jpql.kt
+++ src/DataBase/Jpql.kt
@@ -1,7 +1,8 @@
 package br.com.vraptor.bitBank.banco
 
 class Jpql {
-    /* A JPQL é a linguagem de consulta padrão da especificação JPA! Já a HQL é a linguagem de consulta do Hibernate!
+    /* A JPQL é a linguagem de consulta padrão da especificação JPA!
+    Já a HQL é a linguagem de consulta do Hibernate!
     Isso quer dizer que o JPQL vai funcionar se vc estiver usando o Hibernate ou outra implementação da especificação
     JPA ( como o EclipseLink, por exemplo ). Já a HQL só funcionará se vc estiver usando o Hibernate! E sim, como a HQL
     é de propriedade do Hibernate, ela terá funcionalidades extras além das definidas pela JPQL! */
