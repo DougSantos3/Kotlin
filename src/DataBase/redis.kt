70d993ae038c978d6285c9f790a6c8e3171563e2
douglas <douglasmartinssantos7@gmail.com>
2024-09-02 09:00:35 -0300

    update mongo and redis 2

--- src/DataBase/redis.kt
+++ src/DataBase/redis.kt
@@ -1,15 +1,13 @@
 package br.com.vraptor.bitBank.banco
 
 class Redis {
-    /*
-    Conhecemos o porquê do cache em memória não ser uma boa opção para ambientes produtivos. Sempre que fizer o deploy
-    da aplicação, o cache será apagado, além de não ser escalável;
+    /* Conhecemos o porquê do cache em memória não ser uma boa opção para ambientes produtivos. Sempre que fizer o
+    deploy da aplicação, o cache será apagado, além de não ser escalável.
 
     Entendemos que para ambiente produtivo, utilizar cache distribuído é uma opção melhor, e para isso utilizamos o
-    Redis;
+    Redis.
 
-    Para configurar o Redis devemos configurar a dependência no projeto e subir um contêiner com docker;
+    Para configurar o Redis devemos configurar a dependência no projeto e subir um contêiner com docker.
 
-    Vimos como utilizar o bash do Redis para conseguir monitorar a atividade no contêiner.
-    */
+    Vimos como utilizar o bash do Redis para conseguir monitorar a atividade no contêiner. */
 }
