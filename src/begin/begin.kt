d11c60f0ecc4b45450a1a54f2f3926ba95a546a3
douglas <douglasmartinssantos7@gmail.com>
2024-09-05 11:38:02 -0300

    begin 4

--- src/begin/begin.kt
+++ src/begin/begin.kt
@@ -19,20 +19,20 @@ class Begin {
     o tipo está sendo usado que foi criado lá na super classe Plano. Então precisa transforma em uma classe para ele
     aceitar essa herança.
 
-    Data class ele automaticamente tem o método toString que imprime os atributos  da classe.
+    Data class ele automaticamente tem o método toString que imprime os atributos da classe.
 
 
 
     Tipos primitivos
-    Long 64-92233720368547758089223372036854775807
-    Int 32-21474836482147483647
-    Short 16-3276832767
-    Byte 8-128127
-    Double 6416-17 significant digits (same as float in Python)
-    Float326-7 significant digits
-    Char16UTF-16 code unit (see the section on strings - in most cases, this is one Unicode character,
+    Long: 64-92233720368547758089223372036854775807
+    Int: 32-21474836482147483647
+    Short: 16-3276832767
+    Byte: 8-128127
+    Double: 6416-17 significant digits (same as float in Python)
+    Float: 326-7 significant digits
+    Char16: UTF-16 code unit (see the section on strings - in most cases, this is one Unicode character,
     but it might be just one half of a Unicode character)
-    Boolean true or false
+    Boolean: true or false
     string
 
 
