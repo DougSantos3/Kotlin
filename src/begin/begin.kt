881e002aacd641930034d09f0552d2f92e84b4cc
douglas <douglasmartinssantos7@gmail.com>
2024-09-06 09:44:37 -0300

    begin 4

--- src/begin/begin.kt
+++ src/begin/begin.kt
@@ -9,7 +9,7 @@ class Begin {
     Heranças
     Vimos também que para tornar uma classe herdável é preciso marcá-la com a palavra-chave open porque por padrão,
     todas as classes criadas no Kotlin são automaticamente final, não podendo assim ser herdadas. A marcação final nas
-    classes fica implícita e não precisa ser escrita. Porém, você pode fazer um teste:
+    classes fica implícita e não precisa ser escrita.
     Para sobrescrever o metodo precisa inserir o open nele
 
 
@@ -28,8 +28,7 @@ class Begin {
     Byte: 8-128127
     Double: 6416-17 significant digits (same as float in Python)
     Float: 326-7 significant digits
-    Char16: UTF-16 code unit (see the section on strings - in most cases, this is one Unicode character,
-    but it might be just one half of a Unicode character)
+    Char16: UTF-16 code unit (see the section on strings - in most cases, this is one Unicode character, but it might be just one half of a Unicode character)
     Boolean: true or false
     string
 
@@ -53,12 +52,10 @@ class Begin {
     println(“Titular conta Joao: ${contaJoao.titular}“)
     println(“Titular conta Maria: ${contaMaria.titular}“)
 
-    //Ambos imprimem  Maria
-    //ele aponta para o mesmo primeiro foi escrito Joao depois mudou para Maria
-
+    Ambos imprimem  Maria
+    ele aponta para o mesmo, primeiro foi escrito Joao depois mudou para Maria
     acima sempre é uma estancia se você realmente quizer um novo objeto precisar estanciar
 
     val contaJoao = Conta()
-
     val contaMaria = Conta() */
 }
