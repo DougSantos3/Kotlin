e86d18e90edbe3f3fc69aa6f4e10cf40f9716c4b
douglas <douglasmartinssantos7@gmail.com>
2024-09-10 09:47:29 -0300

    begin2 part 1

---
+++ src/begin/begin2.kt
@@ -0,0 +1,78 @@
+package br.com.vraptor.bitBank.begin
+
+class Begin2 {
+    /*Row string literal: “““   “““
+
+    ele pula  a linha realmente e ajuda na visualização
+
+    “““
+
+    logradouro,
+
+    numero,
+
+    cep,
+
+    cidade “““.trimIdent()
+
+    trimIdent remove as linhas em brancos
+
+
+    equals(): deve devolver true quando for o mesmo objeto e false caso sejam diferentes;
+
+    toString(): deve apresentar o nome da classe e o código do objeto(formato hexadecimal) diferentes para cada objeto
+    br.com.vrapor.bitbank que seria a classe @ EnderecoAfdsd698797d
+
+    hashCode(): deve apresentar o código de cada objeto(formato inteiro) com valores diferentes.
+
+
+
+    ' c' // um caracter(um char)
+
+    “c“ //um texto
+
+    chains(chamada encadeada de safe call): val logradouroNovo: String? = enderecoNulo?.logradouro?
+
+
+
+    let é função mais enxuta sem precisar colocar o if e ela só será executada quando for non-null. Safe call que pode
+    ser combinado com o let.
+
+    enderecoNulo?let {it: Endereco
+
+    println(it.logradouro.length)
+
+
+    late init
+
+
+
+    Na variável, utilizaremos o recurso lateinit para só instanciar o nosso recurso, que vai estar guardado na nossa
+    variável, quando, de fato, for utilizar esse recurso.
+
+    O que isso quer dizer? Não vou inicializar já o meu recurso agora, na hora que declarar na variável. ele só será
+    inicializado quando for chamado nos testes.
+
+    @Autowired
+    private lateinit var topicoRepository: TopicoRepository
+
+
+    Quando utilizamos o lateinit não precisamos passar uma instância para ele já inicializar esse nosso repository,
+    porque vamos fazer isso no momento posterior.
+    }
+
+
+    Sobre carga de metodo
+
+
+
+    somar(valor)
+
+    somar(valor<list>)
+
+    somar(valor, valor2<arrayList>)
+
+
+    Inserindo ssl no site você coloca o certficado de segurança no seu site, mudando de http para https.
+    */
+}
