683412f49b9c671151800023cc456ed8f032a568
douglas <douglasmartinssantos7@gmail.com>
2024-09-17 09:44:35 -0300

    update csrf part 2

--- src/begin/Csrf.kt
+++ src/begin/Csrf.kt
@@ -1,15 +1,14 @@
 package br.com.vraptor.bitBank.begin;
 
 public class Csrf {
-    /* csrf(Cross-Site Request Forgery)
+    /* csrf(Cross-Site Request Forgery/Falsificação de solicitação entre sites)
 
     Você loga no site do seu banco usando o seu navegador preferido.
-
     A credencial do usuário é validada, um cookie é enviado na resposta da requisição HTTP. A partir desse momento, o
     navegador tem salvo no disco o cookie que te mantém autenticado.
 
     Você recebe um e-mail (engenharia social) que te convence a clicar em um link que abre um site arbitrário.
-    Ao entrar nesse site, no corpo dele, tem um formulário assim:
+    Ao entrar nesse site, no corpo dele, tem um formulário.
 
     Esse formulário no site do atacante emula exatamente como o seu banco faz para realizar uma transferência de
     dinheiro.
@@ -17,9 +16,8 @@ public class Csrf {
 
     É daí que vem o nome “Cross-Site Request Forgery”. Foi forjada uma requisição Cross-Site, de um site para outro.
 
-    A requisição forjada não é exatamente o problema, mas sim o fato de o seu navegador ter enviado junto com ela aquele
-    cookie da autenticação e o site do seu banco achará que foi você quem solicitou a transferência.
-
+    O problema real é que essa requisição enviou o cookie da autenticação e o site do seu banco achará que foi você
+    quem solicitou a transferência.
 
     O envio do cookie é feito automaticamente pelo navegador. Quando um cookie é criado, ele possui alguns atributos e
     um deles é o domain que fala ao navegador o domínio onde esse cookie pode ser transacionado. Uma vez que o site do
