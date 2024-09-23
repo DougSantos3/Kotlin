361b3db21a0e68bf992ada07789be03611a684b6
douglas <douglasmartinssantos7@gmail.com>
2024-09-23 10:13:34 -0300

    data class

---
+++ src/DataBase/dataClass.kt
@@ -0,0 +1,11 @@
+package br.com.vraptor.bitBank.begin
+
+class DataClass {
+
+    /* As Data Classes no Kotlin são muito utilizadas quando queremos declarar classes com o objetivo principal de
+    armazenar dados. Uma das vantagens de utilizar data classes é o fato de já trazer alguns métodos implementados que
+     são super úteis durante o desenvolvimento, como:
+
+    equals()
+    toString() */
+}
