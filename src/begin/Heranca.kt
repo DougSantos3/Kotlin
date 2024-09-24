56b51bc69963d80ceb776db9cdbcfe443bd2ea7e
douglas <douglasmartinssantos7@gmail.com>
2024-09-24 09:54:05 -0300

    herança parte 1

---
+++ src/begin/Heranca.kt
@@ -0,0 +1,19 @@
+
+class Herança {
+
+    /*Além das palavras-chave opene final que falamos no Para saber mais: herança e superclasse Any, o Kotlin permite
+    declarar uma classe como abstract. Ao criar uma classe abstrata, informamos que ela passará a ser abstrata e não
+    poderá ser instanciada, assim como se declararmos seus membros como abstratos, estes também não poderão ser
+    implementados.
+
+    Este é um recurso muito útil para garantir o funcionamento correto das estruturas de classes que temos e evitar
+    comportamentos inadequados. Então, como foi o nosso caso no vídeo Conhecendo classes abstratas, quando temos uma
+    classe herdável que não queremos que seja instanciada, podemos substituir o open pelo abstract e assim manter o
+    comportamento desejado.
+
+    Já as classes seladas, ou sealed classes, permitem um nível a mais de segurança e controle sobre a herança. As
+    classes seladas são originalmente abstratas e não permitem que existam subclasses fora do pacote ou módulo em que
+    ela foi criada, limitando as informações que são conhecidas quando ela é compilada.
+
+    Para conhecer mais, acesse a documentação do Kotlin sobre classes abstratas e classes seladas.*/
+}
