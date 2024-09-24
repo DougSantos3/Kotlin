21942f175d20e3a299a450277c59f0f3913c428d
douglas <douglasmartinssantos7@gmail.com>
2024-09-24 09:53:13 -0300

    data class

--- src/begin/dataClass.kt
+++ src/begin/dataClass.kt
@@ -4,8 +4,5 @@ class DataClass {
 
     /* As Data Classes no Kotlin são muito utilizadas quando queremos declarar classes com o objetivo principal de
     armazenar dados. Uma das vantagens de utilizar data classes é o fato de já trazer alguns métodos implementados que
-     são super úteis durante o desenvolvimento, como:
-
-    equals()
-    toString() */
+     são úteis durante o desenvolvimento, como: equals() e toString() */
 }
