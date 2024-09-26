7e1e8e6801756db5b001370b995eadbe33c721a4
douglas <douglasmartinssantos7@gmail.com>
2024-09-26 09:50:10 -0300

    herança parte 3

--- src/begin/Heranca_abstract_sealed.kt
+++ src/begin/Heranca_abstract_sealed.kt
@@ -2,7 +2,7 @@
 class Herança/abstract/sealed {
 
     /* Além das palavras-chave open e final, o Kotlin permite declarar uma classe como abstract. Ao criar uma classe
-    abstrata, informamos que ela passará a ser abstrata e não poderá ser instanciada, assim como se declararmos seus
+    abstrata, informamos que ela passará a ser abstrata e não poderá ser instânciada, assim como se declararmos seus
     membros como abstratos, estes também não poderão ser implementados.
 
     Este é um recurso muito útil para garantir o funcionamento correto das estruturas de classes que temos e evitar
