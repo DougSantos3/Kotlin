c2aeeeb5155dd27587158c2a7322ae18dd4e9d02
douglas <douglasmartinssantos7@gmail.com>
2024-11-01 14:18:23 -0300

    refactor sealed

---
+++ src/begin/sealed.kt
@@ -0,0 +1,3 @@
+/* Já as classes seladas, ou sealed classes, permitem um nível a mais de segurança e controle sobre a herança. As classes
+seladas são originalmente abstratas e não permitem que existam subclasses fora do pacote ou módulo em que ela foi
+criada, limitando as informações que são conhecidas quando ela é compilada. */
