63781e891b5e815db644260f3ece11c83a3b5216
douglas <douglasmartinssantos7@gmail.com>
2024-11-01 14:17:30 -0300

    refactor abstract

--- src/begin/abstract.kt
+++ src/begin/abstract.kt
@@ -3,15 +3,12 @@ package br.com.vraptor.bitBank.begin
 import br.com.vraptor.bitBank.begin.modelo.Cliente
 import br.com.vraptor.bitBank.exception.SaldoInsuficienteExeption
 
-/* Além das palavras-chave open, final, herança e superclasse Any, o Kotlin permite declarar uma classe como abstract.
-Ao criar uma classe abstrata(abstract), informamos que ela passará a ser abstrata e não poderá ser instanciada, assim
+/* Ao criar uma classe abstrata(abstract), informamos que ela passará a ser abstrata e não poderá ser instanciada, assim
 como se declararmos seus membros como abstratos, estes também não poderão ser implementados.
+
 Este é um recurso muito útil para garantir o funcionamento correto das estruturas de classes que temos e evitar
 comportamentos inadequados. Quando temos uma classe herdável que não queremos que seja instanciada, podemos substituir o
-open pelo abstract e assim manter o comportamento desejado.
-Já as classes seladas, ou sealed classes, permitem um nível a mais de segurança e controle sobre a herança. As classes
-seladas são originalmente abstratas e não permitem que existam subclasses fora do pacote ou módulo em que ela foi
-criada, limitando as informações que são conhecidas quando ela é compilada. */
+open pelo abstract e assim manter o comportamento desejado. */
 
 abstract class Abstract(
     titular: Cliente,
