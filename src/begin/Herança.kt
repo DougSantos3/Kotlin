7f68722e4c7d4719ac09a25f6a67fa6f7b59369d
douglas <douglasmartinssantos7@gmail.com>
2024-11-01 14:17:56 -0300

    refactor herança

---
+++ src/begin/Herança.kt
@@ -0,0 +1,6 @@
+/* Kotlin tem um conceito semelhante ao final do Java, mas usa a palavra-chave open para marcar uma classe ou
+método como "aberto" para extensão.
+
+Por padrão, todas as classes e métodos em Kotlin são final — ou seja, não podem ser herdados ou sobrescritos, a menos
+que você declare explicitamente que são open. Isso é o oposto do Java, onde as classes e métodos são "abertos" por
+padrão. */
