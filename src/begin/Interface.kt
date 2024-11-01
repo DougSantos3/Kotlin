9f128fa21a552f46b86b4fbfc6f8ca5ef8e2db1a
douglas <douglasmartinssantos7@gmail.com>
2024-11-01 14:21:46 -0300

    refactor interface

--- src/begin/Interface.kt
+++ src/begin/Interface.kt
@@ -2,7 +2,7 @@ package br.com.vraptor.bitBank.begin
 
 interface interface1 {
 
-    // Recebe uma senha do tipo Int e devolve um Boolean.
+    /*  Recebe uma senha do tipo Int e devolve um Boolean. */
     fun autentica(senha: Int) : Boolean
 
     /* Interface não tem o constructor () depois do nome dele, só teria se fosse classe abstrata.
