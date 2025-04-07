223bd4743ebb0d4d70698dd07b291880e8b08a0a
douglas.santos <douglas.santos@opah.com.br>
2025-04-07 17:39:43 -0300

    update steps abstract part 4

--- src/begin/Abstract.kt
+++ src/begin/Abstract.kt
@@ -32,15 +32,14 @@ abstract class Abstract(
 
     Considerando esse detalhe, aceite o desafio e crie a conta salário que tem tudo que uma conta tem, com exceção do
     comportamento de transferir. Nesta implementação, mantenha a mesma técnica de polimorfismo na transferência para
-    contas que precisam desse comportamento
-    (corrente e poupança).
+    contas que precisam desse comportamento (corrente e poupança).
 
     Para testar o código, tente realizar a transferência com a conta salário e confira se apresenta um problema de
     compilação.
 
     Basicamente, foi utilizada a técnica de herança para mais de uma classe, dessa forma, uma conta pode ser uma conta
     normal(modelo.ContaSalario:  modelo.Conta), como também uma conta que permite a transferência
-    (modelo.ContaCorrente: modelo.ContaTransferivel)
+    (modelo.ContaCorrente: modelo.ContaTransferivel).
 
     Poderia utilizar uma interface, porém, dado que a implementação de transferir é a mesma entre as classes e o
     código altera o saldo, não seria possível ter uma implementação padrão na interface e iria replicar o mesmo código
