dd0ab88d12371384b25038d79a867353ce0c36d4
douglas.santos <douglas.santos@opah.com.br>
2025-04-08 09:24:58 -0300

    update steps abstract part 5

--- src/begin/Abstract.kt
+++ src/begin/Abstract.kt
@@ -3,7 +3,7 @@ package br.com.vraptor.bitBank.begin
 import br.com.vraptor.bitBank.begin.modelo.Cliente
 import br.com.vraptor.bitBank.exception.SaldoInsuficienteExeption
 
-/* Ao criar uma classe abstrata(abstract), informamos que ela passará a ser abstrata e não poderá ser instanciada,
+/* Ao criar uma classe abstract, informamos que ela passará a ser abstrata e não poderá ser instanciada,
 assim como se declararmos seus membros como abstratos, estes também não poderão ser implementados.
 
 Este é um recurso muito útil para garantir o funcionamento correto das estruturas de classes que temos e evitar
