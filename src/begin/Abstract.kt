fe9855cf2cdd655de9946d3ebe7228e10cf1f1c6
Doug Santos <douglasmartinssantos7@gmail.com>
2025-05-14 10:13:12 -0300

    abstract 3

--- src/begin/Abstract.kt
+++ src/begin/Abstract.kt
@@ -3,7 +3,7 @@ package br.com.vraptor.bitBank.begin
 import br.com.vraptor.bitBank.begin.modelo.Cliente
 import br.com.vraptor.bitBank.exception.SaldoInsuficienteExeption
 
-    /* Ao criar uma classe abstract, informamos que ela passará a ser abstrata e não poderá ser instanciada, assim
+    /* Ao criar classe abstract, informamos que ela passará a ser abstrata e não poderá ser instanciada, assim
     como se declararmos seus membros como abstratos, no metodo abstrato estes também não poderão ser implementados,
     apenas quando forem herdados ai poderão sobrescrever os metodos deles.
 
