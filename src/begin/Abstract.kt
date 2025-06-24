70b9f779b1e95b2f3003a7035f5cc6248c83c81a
Doug Santos <douglasmartinssantos7@gmail.com>
2025-06-24 17:57:19 -0300

    abstract 21

--- src/begin/Abstract.kt
+++ src/begin/Abstract.kt
@@ -33,8 +33,8 @@ import br.com.vraptor.bitBank.exception.SaldoInsuficienteExeption
 
     Nesse exemplo, respirar() já vem pronto, e emitirSom() precisa ser implementado pela subclasse.
 
-    Quando temos uma classe que deve ser herdada, mas não deve ser instanciada diretamente, podemos
-    substituí-la de open para abstract, assegurando que o comportamento seja mantido conforme o esperado. */
+    Quando temos uma classe que deve ser herdada, mas não deve ser instanciada diretamente, podemos substituí-la de
+    open para abstract, assegurando que o comportamento seja mantido conforme o esperado. */
 abstract class Conta(
     titular: Cliente,
     numeroDeConta: Int
